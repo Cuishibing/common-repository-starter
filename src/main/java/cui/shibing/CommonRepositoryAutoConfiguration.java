ef19dba1d14a9c9ee32f724efd7e49e5d02ae694
Cui shibing <643237029@qq.com>
2018-12-26 15:26:40 -0800

    添加组合事务管理者类,待完善

--- src/main/java/cui/shibing/CommonRepositoryAutoConfiguration.java
+++ src/main/java/cui/shibing/CommonRepositoryAutoConfiguration.java
@@ -1,7 +1,12 @@
 package cui.shibing;
 
-import cui.shibing.commonrepository.CommonRepositoryImpl;
-import cui.shibing.commonrepository.CommonRepositoryImpl1;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -24,66 +29,62 @@ import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.util.ClassUtils;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import cui.shibing.commonrepository.CommonRepositoryImpl1;
 
 @Configuration
 @ConditionalOnBean(EntityManager.class)
 @AutoConfigureAfter(HibernateJpaAutoConfiguration.class)
-public class CommonRepositoryAutoConfiguration implements BeanFactoryAware, ImportBeanDefinitionRegistrar, EnvironmentAware, ResourceLoaderAware {
+public class CommonRepositoryAutoConfiguration
+		implements BeanFactoryAware, ImportBeanDefinitionRegistrar, EnvironmentAware, ResourceLoaderAware {
 
-    private ResourceLoader resourceLoader;
-    private Environment environment;
+	private ResourceLoader resourceLoader;
+	private Environment environment;
 
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
-    }
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
 
+	@Override
+	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.setEnvironment(this.environment);
+		scanner.setResourceLoader(this.resourceLoader);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
+		List<String> packages = AutoConfigurationPackages.get(this.beanFactory);
+		Set<Class<?>> entityClazzes = new HashSet<>();
+		packages.forEach(p -> {
+			Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents(p);
+			candidateComponents.forEach(c -> {
+				try {
+					entityClazzes.add(ClassUtils.forName(c.getBeanClassName(), resourceLoader.getClassLoader()));
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			});
+		});
+		entityClazzes.forEach(clazz -> {
+			BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+					.rootBeanDefinition(CommonRepositoryImpl1.class);
+			beanDefinitionBuilder.addConstructorArgValue(clazz);
+			// beanDefinitionBuilder.addConstructorArgReference("entityManagerFactory");
+			AbstractBeanDefinition rawBeanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
+			rawBeanDefinition.setLazyInit(true);
+			registry.registerBeanDefinition(clazz.getSimpleName(), rawBeanDefinition);
+		});
 
-    @Override
-    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
-        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(
-                false);
-        scanner.setEnvironment(this.environment);
-        scanner.setResourceLoader(this.resourceLoader);
-        scanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
-        List<String> packages = AutoConfigurationPackages.get(this.beanFactory);
-        Set<Class<?>> entityClazzes = new HashSet<>();
-        packages.forEach(p -> {
-            Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents(p);
-            candidateComponents.forEach(c -> {
-                try {
-                    entityClazzes.add(ClassUtils.forName(c.getBeanClassName(), resourceLoader.getClassLoader()));
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException(e);
-                }
-            });
-        });
-        entityClazzes.forEach(clazz -> {
-            BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(CommonRepositoryImpl1.class);
-            beanDefinitionBuilder.addConstructorArgValue(clazz);
-//            beanDefinitionBuilder.addConstructorArgReference("entityManagerFactory");
-            AbstractBeanDefinition rawBeanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
-            rawBeanDefinition.setLazyInit(true);
-            registry.registerBeanDefinition(clazz.getSimpleName(), rawBeanDefinition);
-        });
+	}
 
-    }
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
 
-    @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
+	private BeanFactory beanFactory;
 
-    private BeanFactory beanFactory;
-
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
-    }
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
 }
